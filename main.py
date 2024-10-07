4bb58ba87e29f28627a15db0ba461e73c1a84d12
aorayden <136974279+aorayden@users.noreply.github.com>
2024-10-07 11:47:42 +0500

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press F9 to toggle the breakpoint.
 
+    for i in range(0, 10):
+        print(i)
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
